7b11acce684b35e062ea1e5b2073694f12caa609
aldoalprak <alprak93@gmail.com>
2020-10-24 23:04:13 +0700

    add promo,detail,category scrapper

--- script.js
+++ script.js
@@ -13,6 +13,78 @@ const mainPageScrap = async(categoryIndex, pageIndex) => {
   return pageResult;
 }
 
+const detailPageScrap = async(url) => {
+  const pageResult = (await get(url)).body; 
+  const $ = cheerio.load(pageResult);
+  const promoArea = $(".area").text();
+  const promoPeriod = $(".periode").text().replace(/(\n)/g, "").replace(/(\t)/g, "");
+  const descImg = `${baseUrl}${$("img",".keteranganinside").attr("src")}`;
+
+  const detailResult = {
+    promoArea,
+    promoPeriod,
+    descImg
+  };
+  return detailResult;
+}
+
+const promoPerPage = async(categoryIndex, pageIndex) => {
+  console.log("==promo page==",`category =>${categoryIndex}`, `page => ${pageIndex}`)
+  const mainPage = await mainPageScrap(categoryIndex, pageIndex);
+  const $ = cheerio.load(mainPage);
+  const promises = [];
+
+  
+  $("#promolain").find("li").each((i, el) => {
+    const detailPath = $("a", el).attr("href");
+    const title = $("#imgClass", el).attr("title");
+    const gridImg = `${baseUrl}/${$("#imgClass", el).attr("src")}`;
+
+    const handleAsync = async() => {
+      const detail = await detailPageScrap(`${baseUrl}/${detailPath}`);
+      
+      const promoInfo = {
+        title,
+        gridImg,
+        ...detail
+      };
+      return promoInfo
+    }
+    promises.push(handleAsync())
+  })
+  
+  const result = (await Promise.all(promises));
+  return result;
+}
+
+const getCategoryList = async() => {
+  const pageScrap = (await get(ccUrl)).body;
+  const $ = cheerio.load(pageScrap);
+  let categoryList = [];
+  const categoriesLength = $("#subcatpromo").find("div").length;
+
+  for(let i = 1; i <= categoriesLength;i++) {
+    const pageScrap = (await get(`${ccUrl}&subcat=${i}`)).body;
+    const $ = cheerio.load(pageScrap);
+
+    const totalPageStr = $("#paging1").attr("title");
+    const OfIndex = totalPageStr.indexOf("of");
+    const totalPage = Number(totalPageStr.slice(OfIndex+2));
+    
+    const categoryTitle = $("img", "#subcatselected").attr("title")
+    
+    const categoryDetail  = {
+      title: categoryTitle,
+      totalPage,
+      categoryIndex: i
+    };
+
+    categoryList.push(categoryDetail);
+  }
+
+  return categoryList;
+}
+
 const main = async() => {
   const startTime = performance.now();
   let categoryList = await getCategoryList();
@@ -34,7 +106,7 @@ const main = async() => {
   const totalTime = performance.now() - startTime;
   console.log(`scrapping time => ${Math.floor(totalTime/1000)} s`);
   const solution = JSON.stringify(result, null, 4);
-  fs.writeFileSync("solution.json", solution);
+  fs.writeFileSync("result.json", solution);
 }
 
 main();
